8e8b43eb42ba964f1e299ec651748c161eb06f34
Autsadakorn Jarernket <autsadakorn.j@kbtg.tech>
2020-10-27 17:34:44 +0700

    test 03

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'BK Library 2' });
 });
 
+router.get('/index2', function(req, res, next) {
+  res.render('index', { title: 'BK Library 5' });
+});
+
 
 module.exports = router;
