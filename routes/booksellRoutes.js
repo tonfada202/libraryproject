078214cd6cc59dd74d2cdd50c1dd1ea0898c6b72
Autsadakorn Jarernket <autsadakorn.j@kbtg.tech>
2020-10-27 18:31:06 +0700

    fdsfdsf

--- routes/booksellRoutes.js
+++ routes/booksellRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const booksellController = require('../controllers/booksellController');
 
-router.get('/booksells',BooksellController.getAllBooksells);
+
+router.get('/booksells', function(req, res, next) {
+    res.render('booksells', { title: 'BK Library 5' });
+  });
 
 router
 .route('/')
